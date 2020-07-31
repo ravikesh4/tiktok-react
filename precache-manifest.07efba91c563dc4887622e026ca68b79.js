bac28734414d8adbfa88ea4043367c2e3fca3f94
Ravikesh Yadav <ravikesh@netzary.com>
2020-07-31 22:55:39 +0530

    Updates

---
+++ precache-manifest.07efba91c563dc4887622e026ca68b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1aa20898b6cb2118ce73f541cebbd82",
+    "url": "/tiktok-react/index.html"
+  },
+  {
+    "revision": "e02ece76d2c404ada120",
+    "url": "/tiktok-react/static/css/main.e2ca98f6.chunk.css"
+  },
+  {
+    "revision": "01963bb0cad6e23f40d1",
+    "url": "/tiktok-react/static/js/2.c9dd4938.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/tiktok-react/static/js/2.c9dd4938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02ece76d2c404ada120",
+    "url": "/tiktok-react/static/js/main.f86392d3.chunk.js"
+  },
+  {
+    "revision": "e602e4420344aebae660",
+    "url": "/tiktok-react/static/js/runtime-main.45a442aa.js"
+  }
+]);
